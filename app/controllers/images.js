098a9d9a27e7640f82b058c56dae0f27441c546f
Daniel Maragno <danielcoelho.esk@gmail.com>
2018-02-09 15:53:51 -0200

    empty key parameter OK

--- app/controllers/images.js
+++ app/controllers/images.js
@@ -9,20 +9,26 @@ module.exports = (app) => {
 	controller.getImages = function(req, res){
 
 		// const key = req.params.key;
-		const key = req.query.key;
+		const key = req.query.key.trim();
 
-		Images
-			.find({'product': {'$regex': key, '$options': 'i'}},{product: true, url: true})
-			.exec()
-			.then(
-				function(images){
-					res.status(200).send(images);
-				},
-				function(err){
-					console.log(err);
-					res.sendStatus(500);
-				}
-			)
+		if(key){
+			Images
+				.find({'product': {'$regex': key, '$options': 'i'}},{product: true, url: true})
+				.exec()
+				.then(
+					function(images){
+						res.status(200).send(images);
+					},
+					function(err){
+						console.log(err);
+						res.sendStatus(500);
+					}
+				)
+		}
+		else {
+			res.status(200).send([]);
+		}
+
 	}
 
 	return controller;
